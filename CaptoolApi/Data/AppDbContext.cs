30e213eaf4923f11dcefa261cb05b7a93907b5ca
Bram Douven <I346784@fhict.nl>
2020-03-23 12:15:48 +0100

    Added EFCore dependencies for Mysql, fixed version inconsistencies.

---
+++ CaptoolApi/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ModelLayer.Models;
+
+namespace Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<User> Users { get; set; }
+
+
+    }
+}
