78db308fe8ac161847ab655cf6e63721e90b2830
Jop <jop.huijbers2001@gmail.com>
2020-06-08 11:23:55 +0200

    added user logic, profile changes

---
+++ CaptoolApi/Logic/Logic/UserLogic.cs
@@ -0,0 +1,30 @@
+﻿using Interfaces.UserInterfaces;
+using ModelLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+
+namespace Logic.Logic
+{
+    public class UserLogic : IUserLogic
+    {
+        private readonly IUserRepos _userRepos;
+
+        public UserLogic(IUserRepos userRepos)
+        {
+            _userRepos = userRepos;
+        }
+
+        public async Task UpdateUser(User oldUser, User newUser)
+        {
+            oldUser.Name = newUser.Name;
+            oldUser.Email = newUser.Email;
+            oldUser.Playlist = newUser.Playlist;
+            oldUser.Password = Crypto.HashPassword(newUser.Password);
+
+            await _userRepos.UpdateAsync(oldUser);
+        }
+    }
+}
